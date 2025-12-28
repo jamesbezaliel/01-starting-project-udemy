9a96a48aa23c624e4852b8f814f6124545059d31
jamesbezaliel <jamesjonathan343@gmail.com>
2025-12-28 15:42:39 +0700

    finalization until the end of section (course 60)

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,26 @@ import { CORE_CONCEPTS, EXAMPLES } from './data.js';
 function App() {
   // we use state to keep track of which tab is selected
   // useState is one of the built-in hooks that React provides (it is used to handle state in functional components)
-  const [selectedTab, setSelectedTab] = useState('components');
+  const [selectedTab, setSelectedTab] = useState();
 
   function handleClick(tabName) {
     setSelectedTab(tabName);
+  }
+
+  // third approach
+  let tabContent = <p>Please select a topic.</p>;
+  if (selectedTab) {
+    tabContent = (
+      <div id='tab-content'>
+        <h3>{EXAMPLES[selectedTab].title}</h3>
+        <p>{EXAMPLES[selectedTab].description}</p>
+        <pre>
+          <code>
+            {EXAMPLES[selectedTab].code}
+          </code>
+        </pre>
+      </div>
+    );
   }
 
   return (
@@ -54,30 +70,58 @@ function App() {
             <CoreConcept2 concept={CORE_CONCEPTS[3]} /> */}
 
             {/* very recommended */}
+            {/* this concept will now be executed by JS for EVERY item in the array, and we'll get the item
+            for every execution as a VALUE for this parameter */}
             {CORE_CONCEPTS.map((concept) => (
               <CoreConcept key={concept.title} {...concept} />
             ))}
+
+            {/* recommendation from instructor */}
+            
           </ul>
         </section>
         <section id='examples'>
           <h2>Examples</h2>
           <menu>
             {/* add tab buttons custom component */}
-            <TabButton onSelect={() => handleClick('components')}>Components</TabButton>
-            <TabButton onSelect={() => handleClick('jsx')}>JSX</TabButton>
-            <TabButton onSelect={() => handleClick('props')}>Props</TabButton>
-            <TabButton onSelect={() => handleClick('state')}>State</TabButton>
+            <TabButton isSelected={selectedTab === 'components'} onSelect={() => handleClick('components')}>Components</TabButton>
+            <TabButton isSelected={selectedTab === 'jsx'} onSelect={() => handleClick('jsx')}>JSX</TabButton>
+            <TabButton isSelected={selectedTab === 'props'} onSelect={() => handleClick('props')}>Props</TabButton>
+            <TabButton isSelected={selectedTab === 'state'} onSelect={() => handleClick('state')}>State</TabButton>
           </menu>
-          <div id='tab-content'>
-            {/* EXAMPLES is taken from ./data.js */}
-            <h3>{EXAMPLES[selectedTab].title}</h3>
-            <p>{EXAMPLES[selectedTab].description}</p>
-            <pre>
-              <code>
-                {EXAMPLES[selectedTab].code}
-              </code>
-            </pre>
-          </div>
+          
+          {/* EXAMPLES is taken from ./data.js */}
+          
+          {/* first approach */}
+          {/* {!selectedTab ? <p>Please select a topic.</p> : null}
+          {selectedTab ? (
+            <div id='tab-content'>
+              <h3>{EXAMPLES[selectedTab].title}</h3>
+              <p>{EXAMPLES[selectedTab].description}</p>
+              <pre>
+                <code>
+                  {EXAMPLES[selectedTab].code}
+                </code>
+              </pre>
+            </div>
+          ) : null } */}
+          
+          {/* second approach */}
+          {/* {!selectedTab ? <p>Please select a topic.</p> : 
+            <div id='tab-content'>
+              <h3>{EXAMPLES[selectedTab].title}</h3>
+              <p>{EXAMPLES[selectedTab].description}</p>
+              <pre>
+                <code>
+                  {EXAMPLES[selectedTab].code}
+                </code>
+              </pre>
+            </div>
+          } */}
+
+          {/* third approach */}
+          {tabContent}
+
         </section>
       </main>
     </div>
@@ -85,3 +129,97 @@ function App() {
 }
 
 export default App;
+
+
+// Latihan Coding 10
+export function ExerciseConditionalContent() {
+    const [warning, setWarning] = React.useState();
+    
+    function handleWarning(selectedWarning) {
+        setWarning(selectedWarning);
+    }
+    
+    let warningData = 
+        <button onClick={() => {handleWarning("Delete")}}>Delete</button>
+      
+      if(warning == 'Proceed') {
+          warningData = 
+          <div>
+            <button onClick={() => {handleWarning("Delete")}}>Delete</button>
+          </div>
+      } else if (warning == 'Delete') {
+        warningData = <div data-testid="alert" id="alert">
+          <h2>Are you sure?</h2>
+          <p>These changes can't be reverted!</p>
+          <button onClick={() => {handleWarning("Proceed")}}>Proceed</button>
+        </div>
+      }
+    
+    return (
+        <div>
+            {warningData}
+        </div>
+    );
+}
+
+// kunci jawaban latihan coding 10
+// don't change the Component name "App"
+export function Jawaban1() {
+    const [isDeleting, setIsDeleting] = React.useState(false);
+    
+    function deleteHandler() {
+        setIsDeleting(true);
+    }
+    
+    function proceedHandler() {
+        setIsDeleting(false);
+    }
+    
+    return (
+      <div>
+        {isDeleting ? <div data-testid="alert" id="alert">
+          <h2>Are you sure?</h2>
+          <p>These changes can't be reverted!</p>
+          <button onClick={proceedHandler}>Proceed</button>
+        </div> : ''}
+        <button onClick={deleteHandler}>Delete</button>
+      </div>    
+    );
+}
+
+// latihan coding 11
+export function Styling() {
+    const [isClicked, setIsClicked] = useState(false);
+    
+    
+    function handleButton() {
+        setIsClicked(true);
+    }
+    
+    return (
+        <div>
+            <p>Style me!</p>
+            <button onClick={handleButton} className={isClicked ? 'active' : ''}>Toggle style</button>
+        </div>
+    );
+}
+
+import React from 'react';
+
+// don't remove the export keyword here!
+export const DUMMY_TODOS = [
+    'Learn React',
+    'Practice React',
+    'Profit!'
+];
+
+// don't change the Component name "App"
+export function DynamicListContent() {
+  return (
+    <ul>
+        {DUMMY_TODOS.map((todoItem) => (
+            <Todo text={todoItem} />
+        ))}
+    </ul>   
+  );
+}
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,9 +1,9 @@
-export default function TabButton({ children, onSelect }) {
+export default function TabButton({ children, onSelect, isSelected }) {
 
   // use it here if you don't want to pass it from App.jsx (such as go to another page)
   // function handleClick() {
   //   console.log(`You clicked the ${children} tab button!`);
   // }
 
-  return (<li><button onClick={onSelect}>{children}</button></li>);
+  return (<li><button className={isSelected ? 'active' : ''} onClick={onSelect}>{children}</button></li>);
 }
